2aedd74a7bc1ed1014171f138da8071c3cea056d
Антон Редкозубов <uacreez@gmail.com>
2021-06-24 16:48:54 +0300

    Попросил работу

--- WeatherAppTest/Service/AnotherCityWeather.swift
+++ WeatherAppTest/Service/AnotherCityWeather.swift
@@ -12,14 +12,15 @@ class AnotherCityWeather {
     var weatherData = WeatherData()
 
     func updateInAnotherCityWeather(in city: String) {
-            let session = URLSession.shared
-            let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)")!
-            let task = session.dataTask(with: url) { data, _, error in
-                if let data = data {
-                    let dataString = String(data: data, encoding: .utf8)
-                    print(dataString)
-                }
+        let session = URLSession.shared
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
+        guard let url = URL(string: urlString) else { return }
+        let task = session.dataTask(with: url) { data, _, error in
+            if let data = data {
+                let dataString = String(data: data, encoding: .utf8)
+                print(dataString)
             }
-            task.resume()
+        }
+        task.resume()
     }
 }
